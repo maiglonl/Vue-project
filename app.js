dd16e6f1ffa69e2dc13d521092ca4f445ef43788
Maiglon <maiglonl@poolseg.com.br>
2016-12-07 16:41:50 -0200

    First project step.

--- app.js
+++ app.js
@@ -1,26 +1,121 @@
 var app = new Vue({
 	el: '#app',
 	data: {
+		nameField: '',
 		title: "Contas a pagar",
+		menus: [
+			{ id:0, name: "Listar contas" },
+			{ id:1, name: "Criar conta" },
+		],
+		activedView: 0,
+		formType: 'insert',
+		bill: {
+			date_due: '',
+			name: '',
+			value: 0,
+			done: 0
+		},
+		names: [
+			'Luz',
+			'Água',
+			'internet',
+			'Mercado',
+			'Cartão de crédito',
+			'Financiamento', 
+			'Gasolina'
+		],
 		bills: [
-			{date_due: '20/0882016', name: 'Conta de luz', value: 30.50, done:1},
-			{date_due: '20/0882016', name: 'Conta de água', value: 30.10, done:0},
-			{date_due: '20/0882016', name: 'Conta de internet', value: 100, done:0},
-			{date_due: '20/0882016', name: 'Conta de Mercado', value: 500, done:0},
-			{date_due: '20/0882016', name: 'Cartão de crédito', value: 1000, done:0},
-			{date_due: '20/0882016', name: 'Financiamento', value: 850, done:0},
-			{date_due: '20/0882016', name: 'Gasolina', value: 500, done:0}
+			{ date_due: '2016-01-01', name: 'Luz', value: 110, done: 1 },
+			{ date_due: '2016-07-20', name: 'Água', value: 110, done: 0 },
+			{ date_due: '2016-07-20', name: 'Mercado', value: 110, done: 0 }
 		]
 	},
 	computed: {
-		status: function(){
+		countBills: function(){
+			var total= 0;
 			var count= 0;
 			for(var b in this.bills){
+				total++;
 				if(!this.bills[b].done){
 					count++;
 				}
 			}
-			return !count ? "Nenhuma conta à pagar" : "Existem "+count+" contas à pagar";
+			if(total == 0){
+				return -1;
+			}
+			return count;
+		}
+	},
+	methods: {
+		showView: function(id){
+			this.activedView = id;
+			if(id == 1){
+				this.formType = 'insert';
+			}
+		},
+		submit: function(){
+			if(this.formType == 'insert'){
+				this.bills.push(this.bill);
+			}
+
+			this.bill = {
+				date_due: '',
+				name: '',
+				value: 0,
+				done: 0
+			};
+
+			this.activedView = 0;
+		},
+		loadBill: function(bill){
+			this.bill = bill;
+			this.activedView = 1;
+			this.formType = 'update';
+		},
+		tooglePayBill: function(bill){
+			this.bill = bill;
+			if(this.bill.done){
+				this.bill.done = 0;
+			}else{
+				this.bill.done = 1;
+			}
+			this.bill = {
+				date_due: '',
+				name: '',
+				value: 0,
+				done: 0
+			};
+		},
+		deleteBill: function(index){
+			if(confirm('Deseja excluir a conta?')){
+				this.bills.splice(index,1);
+			}
+		}
+	},
+	watch: {
+		nameField: function(newName, oldName){
+			console.log(oldName+" -> "+newName);
+		}
+	},
+	filters: {
+		currency: function (value) {
+			if (!value) return ''
+			return "R$ "+value
+		},
+		status: function (value) {
+			return value == 1 ? "Paga" : "Pendente";
+		},
+		dateBr: function (value) {
+			if (!value) return ''
+			date = value.split('-');
+			return date[2]+"/"+date[1]+"/"+date[0];
+		},
+		countToString: function (value) {
+			switch(value){
+				case -1: return "Nenhuma conta cadastrada"; break;
+				case 0: return "Nenhuma conta à pagar"; break;
+				default: return "Existem "+value+" contas à pagar"; break
+			}
 		}
 	}
-})
+});
